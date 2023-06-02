97bae4ab53a7074db2c82109bea21eb29e85187d
njugunamwangi <njugunamwangi.n@gmail.com>
2023-06-02 12:49:50 +0300

    create header & footer components

---
+++ app/View/Components/Layout/Navbar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components\Layout;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Navbar extends Component
+{
+    public array $navigationItems = [];
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('layout.navbar');
+    }
+}
