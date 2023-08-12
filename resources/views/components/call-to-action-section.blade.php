6a86291ff575d56f57dcacdd9b2bf5eb75609e42
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-12 21:15:28 +0300

    call to action section

---
+++ resources/views/components/call-to-action-section.blade.php
@@ -0,0 +1,17 @@
+<x-call-to-action>
+    <x-slot:title>
+        <span class="mb-2 text-base font-semibold text-white">
+            Find Your Next Dream App
+        </span>
+        <h2 class="mb-6 text-3xl font-bold leading-tight text-white sm:mb-8 sm:text-[38px] lg:mb-0" >
+            Get started with <br class="xs:block hidden" />
+            our free trial
+        </h2>
+    </x-slot:title>
+    <x-button-link href="javascript:void(0)" variant="dark" class="mr-4" target="_blank" >
+        Get Pro Version
+    </x-button-link>
+    <x-button-link href="javascript:void(0)" variant="red" target="_blank" >
+        Start Free Trial
+    </x-button-link>
+</x-call-to-action>
