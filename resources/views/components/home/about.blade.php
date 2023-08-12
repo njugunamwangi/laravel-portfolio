049fb2fa9fad3fbfc06abf8a5a1eb2cb593eac70
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-12 21:07:09 +0300

    About section

--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -1,3 +1,59 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+<!-- About Section start -->
+<section id="about" class="overflow-hidden pt-20 pb-12 lg:pt-[120px] lg:pb-[90px] dark:bg-slate-800">
+    <div class="container mx-auto">
+        <div class="-mx-4 flex flex-wrap items-center justify-between">
+            <div class="w-full px-4 lg:w-6/12">
+                <div class="-mx-3 flex items-center sm:-mx-4">
+                    <div class="w-full px-3 sm:px-4 xl:w-1/2">
+                        <div class="py-3 sm:py-4">
+                            <img
+                                src="https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-1.jpg"
+                                alt=""
+                                class="w-full rounded-2xl"
+                            />
+                        </div>
+                        <div class="py-3 sm:py-4">
+                            <img
+                                src="https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-2.jpg"
+                                alt=""
+                                class="w-full rounded-2xl"
+                            />
+                        </div>
+                    </div>
+                    <div class="w-full px-3 sm:px-4 xl:w-1/2">
+                        <div class="relative z-10 my-4">
+                            <img src="https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-3.jpg" alt="" class="w-full rounded-2xl" />
+                            <x-about-dots></x-about-dots>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full px-4 lg:w-1/2 xl:w-5/12">
+                <div class="mt-10 lg:mt-0">
+                    <span class="text-primary mb-2 block text-lg font-semibold">
+                        <blockquote class="text-sm text-gray-500 italic py-2 px-3 border-l-4 border-amber-500" >
+                            "Solve a problem today"
+                        </blockquote>
+                    </span>
+                    <h2 class="text-dark mb-8 text-3xl font-bold sm:text-4xl">
+                        About Me
+                    </h2>
+                    <p class="text-body-color mb-8 text-base">
+                        It is a long established fact that a reader will be distracted by
+                        the readable content of a page when looking at its layout. The point
+                        of using Lorem Ipsum is that it has a more-or-less.
+                    </p>
+                    <p class="text-body-color mb-12 text-base">
+                        A domain name is one of the first steps to establishing your brand.
+                        Secure a consistent brand image with a domain name that matches your
+                        business.
+                    </p>
+                    <x-button-link href="https://github.com/njugunamwangi" variant="primary" target="_blank">
+                        Github
+                    </x-button-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Section end -->
