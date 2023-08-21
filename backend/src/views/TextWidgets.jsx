01781b721d190a94b6a22c2abe2ecc9e5f0f10ca
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-21 22:39:57 +0300

    Add text widget button

--- backend/src/views/TextWidgets.jsx
+++ backend/src/views/TextWidgets.jsx
@@ -1,6 +1,8 @@
 import AdminComponent from "./components/AdminComponent.jsx";
 import {useStateContext} from "./context/ContextProvider.jsx";
 import TextWidgetItem from "./components/TextWidgetItem.jsx";
+import TButton from "./components/core/TButton.jsx";
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
 
 export default function TextWidgets() {
     const { textWidgets } = useStateContext()
@@ -10,7 +12,15 @@ export default function TextWidgets() {
     }
 
     return (
-        <AdminComponent title="Text Widgets">
+        <AdminComponent
+            title="Text Widgets"
+            buttons={(
+                <TButton color="green" to="/categories/create">
+                    <PlusCircleIcon className="w-6 h-6 mr-2" />
+                    Add Text Widget
+                </TButton>
+            )}
+        >
             <ul role="list" className="divide-y divide-gray-100">
                 {textWidgets.map((textWidget) => (
                     <TextWidgetItem textWidget={textWidget} key={textWidget.id} onDeleteClick={onDeleteClick} />
