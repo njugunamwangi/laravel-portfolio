2798e517a8aa747e83c51ee15f71c942f8b62806
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-22 21:34:57 +0300

    Code refactor

--- backend/src/views/context/ContextProvider.jsx
+++ backend/src/views/context/ContextProvider.jsx
@@ -20,7 +20,7 @@ const tmpTextWidgets = [
         name: 'Leslie Alexander',
         email: 'leslie.alexander@example.com',
         role: 'Co-Founder / CEO',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -30,7 +30,7 @@ const tmpTextWidgets = [
         name: 'Michael Foster',
         email: 'michael.foster@example.com',
         role: 'Co-Founder / CTO',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -40,7 +40,7 @@ const tmpTextWidgets = [
         name: 'Dries Vincent',
         email: 'dries.vincent@example.com',
         role: 'Business Relations',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: null,
     },
@@ -49,7 +49,7 @@ const tmpTextWidgets = [
         name: 'Lindsay Walton',
         email: 'lindsay.walton@example.com',
         role: 'Front-end Developer',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -59,7 +59,7 @@ const tmpTextWidgets = [
         name: 'Courtney Henry',
         email: 'courtney.henry@example.com',
         role: 'Designer',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -69,7 +69,7 @@ const tmpTextWidgets = [
         name: 'Tom Cook',
         email: 'tom.cook@example.com',
         role: 'Director of Product',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: null,
     },
@@ -81,7 +81,7 @@ const tmpCategories = [
         name: 'Leslie Alexander',
         email: 'leslie.alexander@example.com',
         role: 'Co-Founder / CEO',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -91,7 +91,7 @@ const tmpCategories = [
         name: 'Michael Foster',
         email: 'michael.foster@example.com',
         role: 'Co-Founder / CTO',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -101,7 +101,7 @@ const tmpCategories = [
         name: 'Dries Vincent',
         email: 'dries.vincent@example.com',
         role: 'Business Relations',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: null,
     },
@@ -110,7 +110,7 @@ const tmpCategories = [
         name: 'Lindsay Walton',
         email: 'lindsay.walton@example.com',
         role: 'Front-end Developer',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -120,7 +120,7 @@ const tmpCategories = [
         name: 'Courtney Henry',
         email: 'courtney.henry@example.com',
         role: 'Designer',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
@@ -130,7 +130,7 @@ const tmpCategories = [
         name: 'Tom Cook',
         email: 'tom.cook@example.com',
         role: 'Director of Product',
-        imageUrl:
+        image_url:
             'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         lastSeen: null,
     },
@@ -139,7 +139,7 @@ const tmpCategories = [
 const tmpProjects = [
     {
         id: 1,
-        imageUrl: '/projects/in2drones.png',
+        image_url: '/projects/in2drones.png',
         title: 'Boost your conversion rate',
         slug: 'lorem-ipsum',
         href: '#',
@@ -151,7 +151,7 @@ const tmpProjects = [
     },
     {
         id: 4,
-        imageUrl: '/projects/portfolio.png',
+        image_url: '/projects/portfolio.png',
         title: 'Boost your conversion rate',
         slug: 'lorem-ipsum',
         href: '#',
@@ -163,7 +163,7 @@ const tmpProjects = [
     },
     {
         id: 2,
-        imageUrl: '/projects/travelwithlewis.png',
+        image_url: '/projects/travelwithlewis.png',
         title: 'Boost your conversion rate',
         slug: 'lorem-ipsum',
         href: '#',
@@ -175,7 +175,7 @@ const tmpProjects = [
     },
     {
         id: 3,
-        imageUrl: '/projects/portfolio.png',
+        image_url: '/projects/portfolio.png',
         title: 'Boost your conversion rate',
         slug: 'lorem-ipsum',
         href: '#',
