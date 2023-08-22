870a82fd4170993dba274dbb2f0cec94a3fa28c3
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-22 13:00:18 +0300

    Project files

---
+++ database/migrations/2023_08_21_182027_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 2000);
+            $table->string('slug', 2000);
+            $table->string('image', 2000);
+            $table->string('projectUrl', 2000);
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
