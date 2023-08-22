78f32c10b8bdb4f4d1183e2dcf6d883a70472311
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-22 14:23:48 +0300

    Text widget files

---
+++ app/Http/Controllers/TextWidgetController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TextWidget;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTextWidgetRequest;
+use App\Http\Requests\UpdateTextWidgetRequest;
+
+class TextWidgetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTextWidgetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TextWidget $textWidget)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTextWidgetRequest $request, TextWidget $textWidget)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TextWidget $textWidget)
+    {
+        //
+    }
+}
