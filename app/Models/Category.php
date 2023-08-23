4a325961a3e9165909c37e3cf2b496d1998c571f
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-23 11:32:26 +0300

    Use Spatie Sluggable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug ;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('category')
+            ->saveSlugsTo('slug');
+    }
 }
