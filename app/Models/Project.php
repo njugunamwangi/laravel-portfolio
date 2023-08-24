0d01973ce019d90f08134c8e2f71bb28c1ab790c
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-24 16:40:08 +0300

    Generate slug from project name

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug ;
 
     protected $fillable = [
         'name',
@@ -19,6 +21,13 @@ class Project extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
     public function categories() : BelongsToMany {
         return $this->belongsToMany(Category::class);
