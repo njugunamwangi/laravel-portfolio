e81e4c20425998a9c6ebb0e2410f3a159308d398
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-24 16:45:21 +0300

    Add loading status

--- backend/src/views/TextWidgets.jsx
+++ backend/src/views/TextWidgets.jsx
@@ -3,30 +3,62 @@ import {useStateContext} from "./context/ContextProvider.jsx";
 import TextWidgetItem from "./components/TextWidgetItem.jsx";
 import TButton from "./components/core/TButton.jsx";
 import { PlusCircleIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+import axiosClient from "../axios.js";
+import Loading from "./components/Loading.jsx";
 
 export default function TextWidgets() {
-    const { textWidgets } = useStateContext()
+    // const { textWidgets } = useStateContext()
+
+    const [ textWidgets, setTextWidgets ] = useState([])
+
+    const [ loading, setLoading ] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axiosClient.get('/textWidget')
+            .then(({ data }) => {
+                setTextWidgets(data.data)
+                setLoading(false)
+            })
+    }, [])
 
     const onDeleteClick = () => {
         console.log("Deleted")
     }
 
     return (
-        <AdminComponent
-            title="Text Widgets"
-            buttons={(
-                <TButton color="green" to="/text-widgets/create">
-                    <PlusCircleIcon className="w-6 h-6 mr-2" />
-                    Create Text Widget
-                </TButton>
-            )}
-        >
-            <ul role="list" className="divide-y divide-gray-100">
-                {textWidgets.map((textWidget) => (
-                    <TextWidgetItem textWidget={textWidget} key={textWidget.id} onDeleteClick={onDeleteClick} />
-                ))}
-            </ul>
-        </AdminComponent>
+        <>
+            <div>
+                {
+                    loading && (
+                        <Loading />
+                    )
+                }
+            </div>
+            <div>
+                {
+                    !loading && (
+                        <AdminComponent
+                            title="Text Widgets"
+                            buttons={(
+                                <TButton color="green" to="/text-widgets/create">
+                                    <PlusCircleIcon className="w-6 h-6 mr-2" />
+                                    Create Text Widget
+                                </TButton>
+                            )}
+                        >
+                            <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                                {textWidgets.map((textWidget) => (
+                                    <TextWidgetItem textWidget={textWidget} key={textWidget.id} onDeleteClick={onDeleteClick}/>
+                                ))}
+                            </div>
+                        </AdminComponent>
+                    )
+                }
+            </div>
+
+        </>
     )
 }
 
---
+++ backend/src/views/components/Loading.jsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+    return (
+        <>
+            <div className="mx-auto max-w-2xl py-32 px sm:py-48 lg:py-56">
+                <div className="text-center">
+                    <div className="text-2xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
+                        Loading...
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
